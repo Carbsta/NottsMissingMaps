093b1b0afd7a23afabeb274105162f5439a43b7a
psyja4 <psyja4@exmail.nottingham.ac.uk>
2019-04-05 01:01:37 +0100

    Editing report card

--- front-end/nott-missing-map/src/components/ReportCard.vue
+++ front-end/nott-missing-map/src/components/ReportCard.vue
@@ -126,11 +126,11 @@ export default {
           `Type: ${this.img.file.type}\n`,
         ];
       } else {
-        return this.img.result.map(rslt => {
+        let unique = [...new Set(return this.img.result.map(rslt => {
           let classifier = rslt.images[0].classifiers[0]
-          let unique = [...new Set([].concat(classifier.classes.filter(oneClass => oneClass.score > 0.5).map(oneClass => `${oneClass.class}`)))]
-          return unique
-        }).reduce((acc, cur) => acc.concat(cur), [])
+          return [].concat(classifier.classes.filter(oneClass => oneClass.score > 0.5).map(oneClass => `${oneClass.class}`))
+        }))]
+        return unique.reduce((acc, cur) => acc.concat(cur), [])
       }
     },
 
