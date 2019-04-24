2c6ea1ece2725a69fe032368784b69e373ed07eb
Yi DING <andy-ding@outlook.com>
2019-04-24 02:37:27 +0100

    Add package discriptions

---
+++ manual-classifier/src/grp25/captcha/package-info.java
@@ -0,0 +1,25 @@
+/**
+ * The main package for the manual classifier, which helps train classifiers.
+ *
+ * <p>
+ *     This manual classifier is build with JavaFX. There are four classes in this
+ *     package, including an entrance class {@link grp25.captcha.Main}, a data wrapper
+ *     {@link grp25.captcha.ImgWithTag}, an app manager {@link grp25.captcha.ClassifierManager}
+ *     and a UI controller {@link grp25.captcha.Controller}.
+ * </p>
+ *
+ * <p>
+ *     As for the functionally of this app, it can segment images into slices while
+ *     attaching inhabitable/non-inhabitable tags to them in the same time. See our
+ *     detailed instruction in our <a href="https://github.com/Carbsta/NottsMissingMaps/tree/master/manual-classifier">GitHub README</a>
+ *     for this manual image classifier.
+ * </p>
+ *
+ * <p>
+ *     This project is a sub-project of UON 2018 Group 25's second year group
+ *     project -- Missing Maps.
+ *     @see <a href="https://github.com/Carbsta/NottsMissingMaps">Our main project repositiory</a>
+ *     @see <a href="https://missingmaps.eu-gb.cf.appdomain.cloud/">Our web application</a>
+ * </p>
+ */
+package grp25.captcha;
